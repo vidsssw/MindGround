eefb01b4edd0b67c7a25d9c7f4d95d59427362b6
vidishawagle <vidishawagle@gmail.com>
2022-02-05 13:11:15 +0530

    setup

---
+++ MindGround/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MindGround.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
